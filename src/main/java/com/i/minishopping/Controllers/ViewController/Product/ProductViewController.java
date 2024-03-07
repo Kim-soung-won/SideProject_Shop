42f9ff174fe1f7f18ceeb5c09c0d0e3833b06aa4
Kim-seng-won <rlatmddnjs0103@naver.com>
2024-03-07 10:09:24 +0900

    Add product Detail Page

--- src/main/java/com/i/minishopping/Controllers/ViewController/Product/ProductViewController.java
+++ src/main/java/com/i/minishopping/Controllers/ViewController/Product/ProductViewController.java
@@ -1,6 +1,8 @@
 package com.i.minishopping.Controllers.ViewController.Product;
 
 import com.i.minishopping.DTO.Product.ProductListResponse;
+import com.i.minishopping.Domains.Product.Product;
+import com.i.minishopping.Services.Product.ProductService;
 import com.i.minishopping.Services.Product.ProductViewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -8,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -16,9 +19,9 @@ import java.util.List;
 public class ProductViewController {
 
     private final ProductViewService productViewService;
+    private final ProductService productService;
     @GetMapping("/productList")
     public String getProductList(Model model){
-        System.out.println("/GET/productList");
         Pageable pageRange = PageRequest.of(0,4);
         List<ProductListResponse> products = productViewService.findAllByPage(pageRange).stream()
                 .map(ProductListResponse::new)
@@ -26,5 +29,11 @@ public class ProductViewController {
         System.out.println(products.get(2).getName());
         model.addAttribute("product",products);
         return "Main/Main";
+    }
+    @GetMapping("/product/{id}")
+    public String getProductDetail(@PathVariable Long id, Model model){
+        Product product = productService.findById(id);
+        model.addAttribute("product",product);
+        return "Main/Detail";
     }
 }
